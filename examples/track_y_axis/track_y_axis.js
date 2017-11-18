fde40d7443f894d419eb7e76148d6c4048648589
Miguel Pignatelli <emepyc@gmail.com>
2017-11-18 23:50:41 +0000

    removed extra function in example

--- examples/track_y_axis/track_y_axis.js
+++ examples/track_y_axis/track_y_axis.js
@@ -109,34 +109,5 @@ var pins_theme = function () {
     	board.start();
     };
 
-    function joinClose (arr, xScale) {
-        var lim = 2;
-        arr.map (function (d) {
-            d._px = xScale(d.pos);
-        });
-        arr.sort (function (a, b) {
-            return a.pos > b.pos;
-        });
-        var groups = [];
-        var currGroup = [arr[0]];
-        var curr = arr[0];
-        for (var i=1; i<arr.length; i++) {
-            if ((arr[i]._px - curr. _px) < lim) {
-                currGroup.push(arr[i]);
-            } else {
-                groups.push (currGroup);
-                currGroup = [arr[i]];
-                curr = arr[i];
-            }
-        }
-        groups.push (currGroup);
-        for (var g=0; g<groups.length; g++) {
-            if (groups[g].length > 1) {
-                var med = groups[g][~~(groups[g].length / 2)];
-                med.label = groups[g].length;
-            }
-        }
-    }
-
     return theme;
 };
